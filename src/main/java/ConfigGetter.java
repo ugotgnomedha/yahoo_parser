cc314c81cb4a97e6b578e6c39697b195e5275a42
ugotgnomedha <nick.sofa71@yandex.ru>
2022-10-17 15:54:21 +0300

    logger & configs added

---
+++ src/main/java/ConfigGetter.java
@@ -0,0 +1,33 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigGetter {
+    private String dbUrl;
+    private String dbUser;
+    private String dbPass;
+
+    public String getDbUrl() {
+        return dbUrl;
+    }
+
+    public void setDbUrl(String dbUrl) {
+        this.dbUrl = dbUrl;
+    }
+
+    public String getDbUser() {
+        return dbUser;
+    }
+
+    public void setDbUser(String dbUser) {
+        this.dbUser = dbUser;
+    }
+
+    public String getDbPass() {
+        return dbPass;
+    }
+
+    public void setDbPass(String dbPass) {
+        this.dbPass = dbPass;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,13 +5,37 @@
 //|  '--'  | /  _____  \  |  |\  \----.   |  |     |  |  |  |
 //|_______/ /__/     \__\ | _| `._____|   |__|     |__|  |__|
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
 
-        // Run unit tests.
-        // ......
+        // Get config values.
+        String configPath = "path/to/config";
+        //String configPath = System.getProperty("log_parser_config");
+        ConfigGetter configGetter = new ConfigGetter();
 
+        try {
+            FileInputStream fis = new FileInputStream(configPath);
+            Properties prop = new Properties();
+            prop.load(fis);
 
+            ///
+            configGetter.setDbUrl(prop.getProperty("DBurl"));
+            configGetter.setDbUser(prop.getProperty("DBuser"));
+            configGetter.setDbPass(prop.getProperty("DBpassword"));
+            ///
+
+            fis.close();
+        } catch (IOException ex){
+            logger.error("Could not parse config file.\n", ex);
+        }
 
     }
 }
